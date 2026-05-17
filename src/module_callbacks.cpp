c5803bdc168041f6bf13ae796264570cfb75f477
MichalG2 <michal.gebka0@gmail.com>
2026-05-17 22:55:13 +0200

    set_pos(), get pos()

--- src/module_callbacks.cpp
+++ src/module_callbacks.cpp
@@ -7,10 +7,19 @@
 #include "konarm_hat.hpp"
 #include "module.hpp"
 #include "module_callbacks.hpp"
+#include "can_messages.h"
 //to do: can timeout??
 void can_callback_set_pos(se::CanBase &can, se::CanDataFrame &received_msg, void *args)
 {
- 
+    (void) can;
+    joint *this_joint=static_cast<joint*>(args);
+
+    can_konarm_1_set_pos_t signals;
+    if(can_konarm_1_set_pos_unpack(&signals, received_msg.data, received_msg.data_size))
+        return; // unpack error
+    this_joint->motor->set_velocity(signals.velocity);
+    this_joint->motor->set_position(signals.position);
+    log_debug("Set position:" + std::to_string(signals.position) + "velocity:" + std::to_string(signals.velocity));
 }
 
 void can_callback_set_torque(se::CanBase &can, se::CanDataFrame &received_msg, void *args)
@@ -27,6 +36,20 @@ void can_callback_set_torque(se::CanBase &can, se::CanDataFrame &received_msg, v
 
 void can_callback_get_pos(se::CanBase &can, se::CanDataFrame &received_msg, void *args)
 {
+    (void) can;
+    (void) received_msg;
+    joint *this_joint=static_cast<joint*>(args);
+
+    can_konarm_1_get_pos_t src_p;
+    se::CanDataFrame send_msg;
+    src_p.position=this_joint->motor->get_position();
+    src_p.velocity=this_joint->motor->get_velocity();
+    send_msg.frame_id  = this_joint->config.can_konarm_get_pos_frame_id; 
+    send_msg.data_size = CAN_KONARM_1_GET_POS_LENGTH;
+    send_msg.fdcan_frame = false;
+    send_msg.extended_id = CAN_KONARM_1_GET_POS_IS_EXTENDED;
+    (void)can_konarm_1_get_pos_pack(send_msg.data, &src_p, send_msg.data_size);
+    (void)can.write(send_msg);
 }
 
 void can_callback_get_torque(se::CanBase &can, se::CanDataFrame &received_msg, void *args)
@@ -37,12 +60,12 @@ void can_callback_get_torque(se::CanBase &can, se::CanDataFrame &received_msg, v
 
     can_konarm_1_get_torque_t src_p;
     se::CanDataFrame send_msg;
-    src_p.torque=this_joint->motor->get_torque(); //float;
+    src_p.torque=this_joint->motor->get_torque();
     send_msg.frame_id    = this_joint->config.can_konarm_get_torque_frame_id; 
     send_msg.data_size   = CAN_KONARM_1_GET_TORQUE_LENGTH;
     send_msg.fdcan_frame = false;
     send_msg.extended_id = CAN_KONARM_1_GET_TORQUE_IS_EXTENDED;
-    can_konarm_1_get_torque_pack(send_msg.data, &src_p, send_msg.data_size);
+    (void)can_konarm_1_get_torque_pack(send_msg.data, &src_p, send_msg.data_size);
     (void)can.write(send_msg);
 }
 void can_callback_status(se::CanBase &can, se::CanDataFrame &received_msg, void *args)
